695a633503468f117fe13e0a5f7bb93f31a71ab3
theRealThomasDavidson <davidson.thomasJ@gmail.com>
2025-01-20 23:29:28 -0500

    chore: update project configuration and add TailwindCSS

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "src"),
       "./runtimeConfig": "./runtimeConfig.browser",
     },
   },
